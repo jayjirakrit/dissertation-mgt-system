6c10dd1856f5acc64742ea65f80365942565bae9
Jirakrit <jayjirakrit@gmail.com>
2024-12-27 11:48:25 +0800

    Fix Store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 
 // Import Bootstrap CSS and JS
 import 'bootstrap/dist/css/bootstrap.min.css';
